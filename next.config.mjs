3a35e2deade8eca7a5840ea38f6d87db2644634c
luna <luna@wvvw.me>
2024-03-03 22:02:09 +1030

    fix: add axiom to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+import { withAxiom } from 'next-axiom';
 
 // Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
 // (when running the application with `next dev`), for more information see:
@@ -10,4 +11,4 @@ if (process.env.NODE_ENV === 'development') {
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withAxiom(nextConfig);
