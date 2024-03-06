59b67f86d154a08a85c9f3a1935d2d8c427e9482
luna <luna@wvvw.me>
2024-03-07 08:09:01 +1030

    fix: upload thing

--- src/upload-thing.ts
+++ src/upload-thing.ts
@@ -1,3 +1,12 @@
 import { UTApi } from 'uploadthing/server';
 
-export const uploadThing = new UTApi();
+export const uploadThing = new UTApi({
+  /*
+   * Cloudflare Workers doesn't support the cache option
+   * so we need to remove it from the request init.
+   */
+  fetch: (url, init) => {
+    if (init && 'cache' in init) delete init.cache;
+    return fetch(url, init);
+  },
+});
